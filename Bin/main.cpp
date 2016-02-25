aaa13cfec38b699033fb6e39124399e531802ecd
engineer-pearl <theadavis101@gmail.com>
2016-02-25 14:26:15 -0800

    Updates for Thur Feb 25 2:25
    Updated Readme
    
    - Notice about switching to Javascript
    
    Updated code:
    
    - Continuing to add the placeholder comments before writing the code itself

--- Bin/main.cpp
+++ Bin/main.cpp
@@ -1,8 +1,9 @@
-/* Start by ... something?
-* Bring up text for admin
-* Check for mods and compare to modlist
-* ask about mods not on modlist
-* ask about mods on modlist but missing
-* Do the overrides
-* Start the connecting
-* Guide the other code
+// 
+// Start by ... something?
+// Bring up text for admin
+// Check for mods and compare to modlist
+// ask about mods not on modlist
+// ask about mods on modlist but missing
+// Do the overrides
+// Start the connecting
+// Guide the other code
