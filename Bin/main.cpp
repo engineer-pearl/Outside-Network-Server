34a407e97723b3d6d4ee4c57ef0f8af01106e514
engineer-pearl <theadavis101@gmail.com>
2016-06-05 23:20:51 -0700

    Preparing for use of full c plus plus
    See client side for better notes.

--- Bin/main.cpp
+++ Bin/main.cpp
@@ -1,5 +1,33 @@
-//Welcome to the program
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+using namespace std;
+
 int main
 {
     
+}
+
+int timed(int timer)
+{
+    condition_variable cv;
+    int value;
+
+    cout << "Please enter the input: ";
+    thread th(read_value);
+
+    mutex mtx;
+    unique_lock<mutex> lck(mtx);
+    while (cv.wait_for(lck, chrono::seconds(timer)) == cv_status::timeout)
+    {
+        cout << "\nTime-Out: 2 second:";
+        cout << "\nPlease enter the input:";
+    }
+    cout << "You entered: " << value << '\n';
+
+    th.join();
+
+    return 0;
 }
